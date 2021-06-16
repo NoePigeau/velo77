0ae190c4977350906caa81c558b8dfa518bf483f
Antoine Roche <aroche10@myges.fr>
2021-06-16 15:05:46 +0200

    Y'a une erreur ça fait crash l'appli

--- frontend/app/src/main/java/com/example/velo77/SignupActivity.java
+++ frontend/app/src/main/java/com/example/velo77/SignupActivity.java
@@ -1,26 +1,27 @@
 package com.example.velo77;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Signup extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private Button btnSignup;
+public class SignupActivity extends AppCompatActivity {
+
+    private Button btnsignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        this.btnSignup = findViewById(R.id.btn_signup);
-        this.btnSignup.setOnClickListener(new View.OnClickListener() {
+        this.btnsignup = findViewById(R.id.btn_signup);
+        this.btnsignup.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent i = new Intent( Signup.this , HomeActivity.class);
+                Intent i = new Intent( SignupActivity.this , HomeActivity.class);
                 startActivity(i);
             }
         });
