4e6ae8f553936b1654c6a6e2166ea28285a8a055
NoePigeau <npigeau@myges.fr>
2021-07-23 15:26:35 +0200

    test

--- frontend/app/src/main/java/com/example/velo77/PanierActivity.java
+++ frontend/app/src/main/java/com/example/velo77/PanierActivity.java
@@ -100,8 +100,9 @@ public class PanierActivity extends AppCompatActivity implements GetAsyncTask.Li
             case "404": Toast.makeText(getApplicationContext(),response,Toast.LENGTH_SHORT).show();
                 break;
             case "410":
+                Toast.makeText(getApplicationContext(),response,Toast.LENGTH_SHORT).show();
                 result.clear();
-                getPanier();
+                //getPanier();
             default:
                 JSONArray json = new JSONArray( response );
                 //this.textView.setText(json.toString());
